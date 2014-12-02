dc17fcb899b8c4d246d33a46232fddee69257e59
Jstillerman <Jason@Jasons-MBP.hsd1.ma.comcast.net>
2014-12-01 19:25:37 -0500

    Added Image Viewer
    Whenever you search for a song, it displays album covers until you find
    one you like. I need to fix the problem that I only download the low
    res version.

--- albumcover.py
+++ albumcover.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import urllib
 import requests
+import easygui
+import os
 
 
 
 while True:
     song = raw_input("Enter Song: ")
     r = requests.get("https://play.google.com/store/search?q=" + song +"&c=music&hl=en")
-    page = r .text
+    page = r.text
     soup = bs(page)
 
     list = []
@@ -16,8 +18,20 @@ while True:
     for img in soup.find_all("img"):
         if img["alt"].lower() == song.lower():
             list.append(img["src"])
-
-    print "Found Image at", list[0]
-    urllib.urlretrieve(list[0], (song + ".jpg"))
+    
+    for i in list:
+        
+        print "Found Image at", i
+        urllib.urlretrieve(i, (song + ".jpg"))
+        image = song +".jpg"
+        msg   = "Do you like this picture?"
+        choices = ["Yes","No","Cancel"]
+        reply=easygui.buttonbox(msg,image=image,choices=choices)
+        if reply == "Cancel":
+            os.remove(song+".jpg")
+            break
+        if reply == "Yes":
+            break
+            
 
 
