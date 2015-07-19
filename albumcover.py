7d218e0e9b0a54a7f0a04d225b7db85576c0586a
= <jason.t.stillerman@gmail.com>
2015-07-19 09:25:56 -0400

    Now uses Google Images

--- albumcover.py
+++ albumcover.py
@@ -8,7 +8,8 @@ import os
 
 while True:
     song = raw_input("Enter Song: ")
-    r = requests.get("https://play.google.com/store/search?q=" + song +"&c=music&hl=en")
+ #   r = requests.get("https://play.google.com/store/search?q=" + song +"&c=music&hl=en")
+    r = requests.get("https://www.google.com/search?q="+song+"album artwork&biw=1280&bih=580&tbs=isz:ex,iszw:1000,iszh:1000&tbm=isch&source=lnt")
     page = r.text
     soup = bs(page)
 
@@ -16,8 +17,8 @@ while True:
 
 
     for img in soup.find_all("img"):
-        if img["alt"].lower() == song.lower():
-            list.append(img["src"])
+       # if img["alt"].lower() == song.lower():
+        list.append(img["src"])
     
     for i in list:
         
